5c0e25182802d9aa7ff16e22c214d8f0bbb47f83
Dukler <iarwain@hotmail.com.ar>
2017-10-29 23:42:29 -0300

    gui

--- Main.py
+++ Main.py
@@ -1,10 +1,14 @@
-import os
-from datetime import datetime
 from FileManager import FileManager
+from kivy.app import App
+
+
+class HelloWorldApp(App):
+    pass
 
 def main():
     fileManager = FileManager("D:/test/1", "D:/test/2")
     fileManager.syncFiles()
+    HelloWorldApp().run()
 
 if __name__ == "__main__":
     main()
