713ddae6c72f145fabe9c58ef60f7c29afb29491
Dukler <iarwain@hotmail.com.ar>
2017-10-30 14:05:05 -0300

    file browser added

--- Main.py
+++ Main.py
@@ -1,13 +1,34 @@
-from FileManager import FileManager
 from kivy.app import App
-
+from os.path import sep, expanduser, isdir, dirname
+from kivy.garden.filebrowser import FileBrowser
+from kivy.utils import platform
 
 class HelloWorldApp(App):
-    pass
+    def build(self):
+        if platform == 'win':
+            user_path = dirname(expanduser('~')) + sep + 'Documents'
+        else:
+            user_path = expanduser('~') + sep + 'Documents'
+        browser = FileBrowser(select_string='Select',
+                              favorites=[(user_path, 'Documents')])
+        browser.bind(
+            on_success=self._fbrowser_success,
+            on_canceled=self._fbrowser_canceled)
+        return browser
+
+    def _fbrowser_canceled(self, instance):
+        print ('cancelled, Close self.')
+
+    def _fbrowser_success(self, instance):
+        print (instance.selection)
+
+
+def _fbrowser_success(self, instance):
+    print (instance.selection)
 
 def main():
-    fileManager = FileManager("D:/test/1", "D:/test/2")
-    fileManager.syncFiles()
+    #fileManager = FileManager("D:/test/1", "D:/test/2")
+    #fileManager.syncFiles()
     HelloWorldApp().run()
 
 if __name__ == "__main__":
